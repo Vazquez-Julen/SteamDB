f02062bf5acb42acb6031dd4d33e18dd60b1d0a9
Julen <vazquez.julen@uni.eus>
2026-01-08 12:17:29 +0100

    .

---
+++ steamdb/steamdb/src/main/java/open/data/Main.java
@@ -0,0 +1,33 @@
+package open.data;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("/open/data/SteamDBP.fxml")
+        );
+
+        Scene scene = new Scene(loader.load(), 1000, 600);
+
+        stage.setTitle("SteamDB");
+        stage.getIcons().add(
+            new Image(getClass().getResourceAsStream("/open/data/SteamDBLogo.png"))
+        );
+
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
